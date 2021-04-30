dccf72eda680269fe4f36c3b72ef9432f04feea3
Povilas Brilius <pbrilius@gmail.com>
2021-04-30 18:58:33 +0300

    Task assemble.

--- src/DataFixtures/TaskFixtures.php
+++ src/DataFixtures/TaskFixtures.php
@@ -4,13 +4,21 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Task;
 
 class TaskFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 16; $i++) {
+            $task = new Task();
+            $task->setTitle(hash('md5', random_bytes(5)));
+            $task->setComment(hash('md5', random_bytes(6)));
+            $task->setDateStarted(new \DateTime());
+            $task->setTimeSpent(new \DateInterval('PT5M'));
+
+            $manager->persist($task);
+        }
 
         $manager->flush();
     }
